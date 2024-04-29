affd9636ef537078233f240f254f27b79d73ce81
Abhijith V A <abhijithva2001@gmail.com>
2024-04-29 17:23:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+//IMPORT ROUTES
+const authRoute = require('./routes/auth')
+
+dotenv.config();
+
+// CONNECT DB
+mongoose.connect(process.env.DB_CONNECT);
+
+// Check if the connection is successful
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB database");
+});
+
+//MIDDLEWARES
+app.use(express.json());
+
+
+
+//ROUTE MIDDLEWARES
+app.use('/api/user',authRoute);
+
+app.listen(3000, ()=> console.log(`server running `))
